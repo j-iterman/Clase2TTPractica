3f86816538bcb9760b28210c625d8b03111fec81
Josefina <josefina.iterman@mercadolibre.com>
2021-01-25 10:44:43 -0300

    Modificación en excepción que lanza setValue

--- src/password/Password.java
+++ src/password/Password.java
@@ -1,6 +1,5 @@
 package password;
 
-import java.util.InputMismatchException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +12,10 @@ public class Password {
         this.patron = Pattern.compile(regex);
     }
 
-    public void setValue(String pwd) throws InputMismatchException{
+    public void setValue(String pwd) throws Exception {
 
         Matcher mat = patron.matcher(pwd);
-        if (!mat.matches()) throw new InputMismatchException();
+        if (!mat.matches()) throw new Exception("La contraseña no cumple las condiciones");
 
         this.password = pwd;
 
